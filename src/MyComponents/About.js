cd97d4787bc90585efadc8539e547728a909b8ca
Krunal Ashishbhai Kishnadwala <87028427+Mrkk1008@users.noreply.github.com>
2021-08-26 19:45:29 +0530

    Soure code added

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h3 className="text-center my-3">
+       This Site is developed by Krunal Ashishbhai Kishnadwala.
+      </h3>
+    </div>
+  );
+};
